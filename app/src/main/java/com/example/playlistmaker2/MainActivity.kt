3aec0a27d760a876967677ab95960c12687a205b
NikAndreev333 <white.green.list@gmail.com>
2024-03-01 04:58:13 +0300

    2

--- app/src/main/java/com/example/playlistmaker2/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker2/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.playlistmaker2
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+
+import android.widget.Button
+import android.widget.Toast
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val searchButton = findViewById<Button>(R.id.button_search)
+        val libraryButton = findViewById<Button>(R.id.button_library)
+        val settingsButton = findViewById<Button>(R.id.button_settings)
+
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonClickListener)
+        libraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+        }
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
         }
     }
+
+
 }

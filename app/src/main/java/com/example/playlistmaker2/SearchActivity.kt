75ea95a002de0682783f10eaadce09412c30f62e
NikAndreev333 <white.green.list@gmail.com>
2024-04-22 02:54:18 +0300

    ПЗ 11 спринт

--- app/src/main/java/com/example/playlistmaker2/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker2/SearchActivity.kt
@@ -6,37 +6,72 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker2.R.id
+import com.example.playlistmaker2.R.layout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity: AppCompatActivity() {
     private lateinit var editText: EditText
     private lateinit var clearButton: ImageView
+    private lateinit var trackList: ArrayList<Track>
+    private lateinit var trackListAdapter: TrackAdapter
+    private lateinit var trackRV: RecyclerView
+    private lateinit var notFoundPlaceholder: LinearLayout
+    private lateinit var noConnectionPlaceholder: LinearLayout
+    private lateinit var updateButton: Button
     private var request: String? = SEARCH_STRING
+    private val baseItunesURL = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseItunesURL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
+    private val iTunesAPI = retrofit.create(ITunesAPI::class.java)
 
-
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(layout.activity_search)
 
-        editText = findViewById<EditText>(R.id.edit_text)
-        clearButton = findViewById<ImageView>(R.id.clearSearchButton)
+        editText = findViewById<EditText>(id.edit_text)
+        clearButton = findViewById<ImageView>(id.clearSearchButton)
         clearButton.visibility = View.GONE
+        notFoundPlaceholder = findViewById<LinearLayout>(R.id.notFoundPlaceholder)
+        noConnectionPlaceholder = findViewById<LinearLayout>(R.id.noConnectionPlaceholder)
+        updateButton = findViewById<Button>(R.id.updateButton)
+        trackRV = findViewById<RecyclerView>(R.id.trackRV)
+        trackList = arrayListOf()
+        trackListAdapter = TrackAdapter(trackList)
 
-        val backToMain = findViewById<ImageButton>(R.id.backButtonSearchActivity)
+
+
+
+        val backToMain = findViewById<ImageButton>(id.backButtonSearchActivity)
         backToMain.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
         clearButton.setOnClickListener {
             editText.setText("")
+            trackList.clear()
+            trackRV.isVisible = false
+            trackListAdapter.notifyDataSetChanged()
             hideKeyboard()
         }
         val textWatcher = object : TextWatcher {
@@ -55,34 +90,68 @@ class SearchActivity: AppCompatActivity() {
         }
         editText.addTextChangedListener(textWatcher)
 
-        val recycler =findViewById<RecyclerView>(R.id.trackRV)
-        val trackList:List<Track> = listOf(
-            Track("Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-        )
-        recycler.layoutManager = LinearLayoutManager(this)
-        val trackListAdapter = TrackAdapter(trackList)
-        recycler.adapter = trackListAdapter
-    }
 
+
+
+        trackRV.layoutManager = LinearLayoutManager(this)
+
+        trackRV.adapter = trackListAdapter
+
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId ==     EditorInfo.IME_ACTION_DONE) {
+                trackSearch()
+                true
+            }
+            false
+        }
+        updateButton.setOnClickListener {
+            trackList.clear()
+            noConnectionPlaceholder.isVisible = false
+            trackSearch()
+            trackListAdapter.notifyDataSetChanged()
+
+
+        }
+    }
+    private fun showNotFoundPlaceholder() {
+        notFoundPlaceholder.isVisible = true
+    }
+    private fun showNoConnectionPlaceholder () {
+        noConnectionPlaceholder.isVisible = true
+    }
+    private fun trackSearch() {
+        iTunesAPI.search(editText.text.toString()).enqueue(object : Callback<TrackResponse> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(call: Call<TrackResponse>, response: Response<TrackResponse>) {
+                if (response.code() == 200){
+
+                    if (response.body()?.results?.isNotEmpty() == true){
+                        trackList.clear()
+                        trackRV.isVisible = true
+                        trackList.addAll(response.body()?.results!!)
+                        if (trackList.isNotEmpty()) {
+                            val text = "Пора покормить кота!"
+                            val duration = Toast.LENGTH_SHORT
+
+                            val toast = Toast.makeText(applicationContext, text, duration)
+                            toast.show()
+                        }
+                        trackListAdapter.notifyDataSetChanged()
+
+
+                    } else {
+                        showNotFoundPlaceholder()
+                    }
+                } else {
+                    showNoConnectionPlaceholder()
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                showNoConnectionPlaceholder()
+            }
+        })
+    }
     private fun hideKeyboard () {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         inputMethodManager?.hideSoftInputFromWindow(editText.windowToken, 0)
